b6fe8dd8dfde33cae48be21daf6b914391a5d57f
infiniteSky <bolab@foxmail.com>
2019-05-30 11:56:52 +0800

    修改响应数据格式

--- run.py
+++ run.py
@@ -1,5 +1,6 @@
 import json
 import time
+import random
 from SimpleWebSocketServer import SimpleWebSocketServer, WebSocket
 
 ACTIONS = ['get', 'set', 'ping']
@@ -13,8 +14,9 @@ def parse_request(bundle):
     return result
 
 
-def make_response(errno, msg, data=None, str_mode=False):
+def make_response(type, errno, msg, data=None, str_mode=False):
     response = {
+        'type': str(type),
         'error': errno,
         'msg': msg,
         'data': '' if data is None else data
@@ -27,24 +29,23 @@ def do_action(inst, bundle):
     try:
         action = bundle['action']
     except Exception as e:
-        inst.sendMessage(make_response(-1, 'Bad request'))
+        inst.sendMessage(make_response('common', -1, 'Bad request'))
     if action is not None:
         if action not in ACTIONS:
-            inst.sendMessage(make_response(-1, 'Unknown action'))
+            inst.sendMessage(make_response('common', -1, 'Unknown action'))
         else:
             try:
                 if action == 'get':
-                    inst.sendMessage(make_response(0, 'success', {
-                        'type': 'temp',
+                    inst.sendMessage(make_response('temp', 0, 'success', {
                         'id:': 1,
-                        'value': 21.68
+                        'value': random.randint(0, 1000) / 10
                     }))
                 elif action == 'set':
-                    inst.sendMessage(make_response(0, 'success'))
+                    inst.sendMessage(make_response('set', 0, 'success'))
                 elif action == 'ping':
-                    inst.sendMessage(make_response(0, 'pong'))
+                    inst.sendMessage(make_response('pong', 0, 'pong'))
             except Exception:
-                inst.sendMessage(make_response(-1, 'Missing argument'))
+                inst.sendMessage(make_response('common', -1, 'Missing argument'))
 
 
 class CoreServer(WebSocket):
@@ -52,7 +53,7 @@ class CoreServer(WebSocket):
     def handleMessage(self):
         data = parse_request(self.data)
         if data is None:
-            self.sendMessage(make_response(-1, 'Invalid request'))
+            self.sendMessage(make_response('common', -1, 'Invalid request'))
         else:
             do_action(self, data)
 
