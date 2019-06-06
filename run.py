e451f3d066b5141446aeea727da50779984c526d
infiniteSky <boxlab@foxmail.com>
2019-06-06 22:16:16 +0800

    所有功能完成（突然冒出）

--- run.py
+++ run.py
@@ -11,11 +11,16 @@ DHT_HOLDER = dht11.DHT11(pin=1)
 last_read = 0
 curr_temp = 0
 curr_humi = 0
+switchs = [6, 13, 19, 26]
+sw_state = [0, 0, 0, 0]
 
 # initialize GPIO
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BCM)
 GPIO.cleanup()
+
+GPIO.setup(switchs, GPIO.OUT)
+GPIO.output(switchs, 1)
 
 
 def parse_request(bundle):
@@ -69,18 +74,24 @@ def do_action(inst, bundle):
                             'updated': last_read,
                             'value': curr_humi
                         }))
-                    elif data['type'] == 'illu':
-                        inst.sendMessage(make_response('illu', 0, 'success', {
-                            'id:': 1,
-                            'updated': last_read,
-                            'value': random.randint(0, 1000)
-                        }))
+                    elif data['type'] == 'swstate':
+                        inst.sendMessage(make_response('swstate', 0, 'success', sw_state))
+                    else:
+                        inst.sendMessage(make_response('common', -1, 'Unknown sensor'))
                 elif action == 'set':
+                    index = (int(data['id']) - 1)
+                    sw_state[index] = int(data['state'])
+
+                    pin = switchs[index]
+                    state = 0 if sw_state[index] == 1 else 1
+
+                    GPIO.output(pin, state)
+                    print('Pin: %s | State: %s' % (pin, state))
                     inst.sendMessage(make_response('set', 0, 'success'))
                 elif action == 'ping':
                     inst.sendMessage(make_response('pong', 0, 'pong'))
             except Exception:
-                inst.sendMessage(make_response('common', -1, 'Missing argument'))
+                inst.sendMessage(make_response('common', -1, 'Argument error'))
 
 
 class CoreServer(WebSocket):
